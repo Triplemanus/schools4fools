463f8f20c5d76bab2e035ac5c01ce6e7324969cc
Jon O'Drobinak <jodrobinak@gmail.com>
2019-11-05 16:19:34 -0700

    Add additional fields to card details, styling and linting

--- src/Containers/SchoolCardDetails.js
+++ src/Containers/SchoolCardDetails.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './SchoolCardDetails.scss';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export const SchoolCardDetails = ({  school_name, address, phone, distance, returnRoute }) => {
-  console.log('S_C_D state is : ', school_name);
+export const SchoolCardDetails = ({ schools, error, returnRoute}) => {
+
+
+// export const SchoolCardDetails = ({  school_name, address, phone, distance, returnRoute }) => {
+  console.log('S_C_D state, schoolId is : ', schools);
   return (
     <section className="CardDetails">
       <Link to={`${returnRoute}`} className='back-btn'>◀ back</Link>
       <article id="cardDetails-container">
-      <h1 id="school-name">{school_name}</h1>
-      <h2 id="address">{address}</h2>
-      <p className='card-phone'>{phone}</p>
-      <p className='card-distance'>{distance}</p>
+      <h1 id="school-name">School Name:  {schools.schoolList[1].schoolName}</h1>
+      <h2 id="school-address">School Address:  {schools.schoolList[1].address.street}</h2>
+      <p className='card-phone'>School Phone:  {schools.schoolList[1].phone}</p>
+      <p className='card-distance'>Low Grade:  {schools.schoolList[1].lowGrade}</p>
       </article>
     </section>
   )
@@ -25,10 +28,10 @@ export const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(SchoolCardDetails);
 
-SchoolCardDetails.propTypes = {
-  school_name: PropTypes.string.isRequired,
-  address: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired,
-  distance: PropTypes.number,
-  returnRoute: PropTypes.string.isRequired
-}
+// SchoolCardDetails.propTypes = {
+//   school_name: PropTypes.string.isRequired,
+//   address: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired,
+//   distance: PropTypes.number,
+//   returnRoute: PropTypes.string.isRequired
+// }
